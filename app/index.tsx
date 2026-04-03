63a60ded6d89121a815b725d68ae728742eb4318
Nischal Shrestha <nnischal9156@gmail.com>
2026-04-03 19:48:05 +0545

    feat: nativewind integrated successfully

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-800">
+    <View className="flex-1 items-center justify-center bg-red-100">
       <Text className="text-3xl font-bold text-blue-600">
         NativeWind Working
       </Text>
